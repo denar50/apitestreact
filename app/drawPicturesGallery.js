73893e8740fcc9461760344e9e58b2324e459734
Denar <lobotk50@gmail.com>
2015-11-02 06:54:04 -0500

    Styles enhanced, transitions added to the picture elements

--- app/drawPicturesGallery.js
+++ app/drawPicturesGallery.js
@@ -2,16 +2,41 @@ var d3 = require('d3');
 function drawPicturesGallery(pictures)
 {
   var picturesContainer = d3.select('#pictures-container');
-  picturesContainer.selectAll('img').remove();
+  picturesContainer.selectAll('.image-result').remove();
   pictures = pictures.filter(function(picture){
     return picture.url_m;
   });
 
-  picturesContainer.selectAll('img').data(pictures).enter().append('img').attr('src', function(d){
+  picturesContainer.selectAll('.image-result')
+  .data(pictures)
+  .enter()
+  .append('div')
+  .classed('image-result', true)
+  .append('img')
+  .attr('src', function(d){
     return d.url_m;
-  }).classed('hidden', true).on('load', function() {
+  })
+  .classed('hidden', true).
+  on('load', function() {
       d3.select(this).classed('hidden', false);
+      appendEvents(this);
   });
+
+
+  function appendEvents(image)
+  {
+    image = d3.select(image);
+    image.on('click', function(){//Click for mobile
+      d3.select(this).classed("hovered", true);
+    })
+    image.on('mouseover', function(){
+      d3.select(this).classed("hovered", true);
+    })
+    .on('mouseleave', function(){
+      console.log("Mouse leave!");
+      d3.select(this).classed("hovered", false);
+    });
+  }
 
 }
 
