c2dfe11405bb9beafc866ea01f0bf0a15d7dd21a
Denar <lobotk50@gmail.com>
2015-11-02 22:59:40 -0500

    PageControl component tests finished

--- __tests__/components/PageControl.js
+++ __tests__/components/PageControl.js
@@ -6,19 +6,94 @@ jest.dontMock(APP_UTILS_PATH);
 
 describe('PageControl react component', function(){
   var React = require('react');
-  //var ReactDOM = require('react-dom');
-  //var TestUtils = require('react-addons-test-utils');
-  //var PageControl = require(PAGE_CONTROL_PATH);
-  //var Utils = require(APP_UTILS_PATH);
+  var ReactDOM = require('react-dom');
+  var TestUtils = require('react-addons-test-utils');
+  var PageControl = require(PAGE_CONTROL_PATH);
+  var Utils = require(APP_UTILS_PATH);
 
-  var pageControlInstance;
+  var pageControlInstance, pagesLinkElements;
+  //Mocks
+  var mocksObject = {
+    onPageChange : function(page){}
+  };
 
-  /*beforeEach(function(){
-    pageControlInstance = TestUtils.renderIntoDocument(<PageControl />);
-  });*/
-
-  it('should be defined', function()
+  function generateTestCase(page, pages, offset)
   {
-    expect(1).toBe(1);
+    pageControlInstance = TestUtils.renderIntoDocument(<PageControl page={page} pages={pages} offset={offset} onPageChange={mocksObject.onPageChange} />);
+    pagesLinkElements = TestUtils.scryRenderedDOMComponentsWithClass(pageControlInstance, 'page-link');
+  }
+
+
+
+  describe('A component that updates the page correctly', function(){
+    beforeEach(function(){
+      spyOn(mocksObject, 'onPageChange');
+      generateTestCase(5, 20, 2);
+    });
+
+    it('Should render the right amount of links', function()
+    {
+      expect(pagesLinkElements.length).toBe(7); //two links on each side of the 5 and the two arrows
+    });
+
+    it('Should call the onPageChange method with the right value when the left arrow is pressed', function()
+    {
+      TestUtils.Simulate.click(pagesLinkElements[0]);
+      setTimeout(function(){
+        expect(mocksObject.onPageChange).toHaveBeenCalledWith(4); //one position to the left from page 5
+      }, 0);
+      jest.runAllTimers();
+    });
+
+    it('Should call the onPageChange method with the right value when the right arrow is pressed', function()
+    {
+      TestUtils.Simulate.click(pagesLinkElements[pagesLinkElements.length - 1]);
+      setTimeout(function(){
+        expect(mocksObject.onPageChange).toHaveBeenCalledWith(6); //one position to the right from page 5
+      }, 0);
+      jest.runAllTimers();
+    });
+
+    it('Should call the onPageChange method with the right value for page 3', function()
+    {
+      TestUtils.Simulate.click(pagesLinkElements[1]);
+      setTimeout(function(){
+        expect(mocksObject.onPageChange).toHaveBeenCalledWith(3); //Click on page 3
+      }, 0);
+      jest.runAllTimers();
+    });
   });
+
+
+  it('Should display the right amount of elements when the offset is not defined (4 is the default)', function()
+  {
+    generateTestCase(5, 20);
+    expect(pagesLinkElements.length).toBe(10); //4 links on the left of page 5 and 4 links plus the right arrow on the right.
+  });
+
+  it('Should display the right amount of elements when the page equals the last page', function()
+  {
+    generateTestCase(20, 20);
+    expect(pagesLinkElements.length).toBe(10); //8 links on the left of page 20 and the left arrow
+  });
+
+  it('Should display the right amount of elements when the page equals the first page', function()
+  {
+    generateTestCase(1, 20);
+    expect(pagesLinkElements.length).toBe(10); //8 links on the right of page 1 and the right arrow
+  });
+
+  it('Should display zero links when the page param is falsy', function()
+  {
+    generateTestCase(0, 20);
+    expect(pagesLinkElements.length).toBe(0);
+  });
+
+  it('Should display zero links when the pages param is falsy', function()
+  {
+    generateTestCase(5, 0);
+    expect(pagesLinkElements.length).toBe(0);
+  });
+
 });
+//Node v0.10.40
--- app/components/PageControl.jsx
+++ app/components/PageControl.jsx
@@ -3,7 +3,7 @@ var Utils = require('../utils/Utils');
 
 var PageControl = React.createClass({
   /**
-  * Event handler for when the user requests a new page. 
+  * Event handler for when the user requests a new page.
   * It calculates (if needed) the requested page and passes it to the hanlder defined in the props.
   *
   * @param {string} page the requested page
@@ -24,7 +24,7 @@ var PageControl = React.createClass({
       this.props.onPageChange(page);
     }
   },
-  
+
   /**
   * Given the current page this will render the pagination links and will try to put the current page in the middle of them. The props.offset
   * is optional and it represents the amount of links that will be displaed on each side of the current page. E.g: If the offset is 3, props.page is 5 and props.pages is 10 then
@@ -34,7 +34,7 @@ var PageControl = React.createClass({
   {
     var links = [];
     if(this.props.page && this.props.pages)
-    {  
+    {
       var currentPage = this.props.page;
       var right = currentPage;
       var left = currentPage;
@@ -45,7 +45,7 @@ var PageControl = React.createClass({
       var pages = [];
       pages.push(currentPage);
       while((left >= leftLimit || right <= rightLimit) && numberOfLinksToShow > 0)
-      { 
+      {
         if(--left >= leftLimit)
         {
           pages.unshift(left);
@@ -58,15 +58,15 @@ var PageControl = React.createClass({
         }
       }
       var links = pages.map(function(currentPage){
-        return <a href="#" className={currentPage === this.props.page ? 'selected' : ''} onClick={this.onPageChange.bind(this, currentPage)} key={currentPage}>{currentPage}</a>
+        return <a href="#" className={'page-link ' + (currentPage === this.props.page ? 'selected' : '')} onClick={this.onPageChange.bind(this, currentPage)} key={currentPage}>{currentPage}</a>
       }.bind(this));
-      if(this.props.page - 4 > 1)
+      if(this.props.page - offset > 1)
       {
-        links.unshift(<a href="#" onClick={this.onPageChange.bind(this, "<")} key="<">&lt;</a>);
+        links.unshift(<a href="#" className='page-link' onClick={this.onPageChange.bind(this, "<")} key="<">&lt;</a>);
       }
-      if(this.props.page + 4 < this.props.pages)
+      if(this.props.page + offset < this.props.pages)
       {
-        links.push(<a onClick={this.onPageChange.bind(this, ">")} href="#" key=">">&gt;</a>);
+        links.push(<a className='page-link' onClick={this.onPageChange.bind(this, ">")} href="#" key=">">&gt;</a>);
       }
     }
     return <div className="paginator">{links}</div>
